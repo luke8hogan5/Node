9fad429d033c93fbbf55d480f2e0a03213c27828
luke8hogan5 <luke8hogan5@gmail.com>
2020-09-09 18:04:57 +0100

    Test Commit

--- Events.js
+++ Events.js
@@ -1,10 +1,9 @@
-const EventEmitter = require('Node/Events');
+const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter { }
 
 const myEmitter = new MyEmitter();
 
-//Event Listner
 myEmitter.on('event', () => console.log("Event Fired"));
 
 myEmitter.emit('event');
